3cba816bad7ad05c10f9a904f076573fdee60f6d
Lokesh1123 <113119973+Lokesh1123@users.noreply.github.com>
2023-07-11 20:44:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,7 @@ except URLError as e:
 streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 #Snowflake-related functions
 def get_fruit_load_list():
    with my_cnx.cursor() as my_cur:
