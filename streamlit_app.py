37bed27b29acb2f677b295c677a765e44a8e75e2
Lokesh1123 <113119973+Lokesh1123@users.noreply.github.com>
2023-07-12 11:41:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,55 +1,59 @@
 import streamlit
-import requests
 import pandas
-from urllib.error import URLError
+import requests
 import snowflake.connector
-
-streamlit.title('My Parents New Healthy Diner')
-
+from urllib.error import URLError
+streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('  🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('  🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('  🐔 Hard-Boiled Free-Range Egg')
-
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacoda Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.dataframe(my_fruit_list)
-
-# Let's put a pick list here so they can pick the fruit they want to include
-fruits = ['Avocado', 'Strawberries']
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page
 streamlit.dataframe(fruits_to_show)
-
-#create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
-   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-   return fruityvice_normalized 
-#New section to display fruityvice api response
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-   if not fruit_choice:
-        streamlit.error("Please select a fruit to get information.")
-   else:  
-        back_form_function = get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_form_function)
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("please select a fruit to get information.")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
-
-streamlit.stop()
-
+#import snowflake.connector
 streamlit.header("The fruit load list contains:")
-
-#Snowflake-related functions
 def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+       my_cur.execute("SELECT * from fruit_load_list")
+       return my_cur.fetchall()
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+   my_cur.execute("INSERT INTO fruit_load_list VALUES ('" + new_fruit + "')")
+   return "Thanks for adding"  +  new_fruit
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button ('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+streamlit.stop()
    with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
